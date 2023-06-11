a25ea97ea50eba37349635dcc65e625744a3b803
sirvince <atunwavincent1999@gmail.com>
2023-06-11 17:49:03 +0100

    Subsequent commit:update to responsive header

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// const navMenu = document.getElementById('navMenu'),
+//     navToggle = document.getElementById('navToggle'),
+//         navClose= document.getElementById('navClose')
+
+// if(navToggle){
+//     navToggle.addEventListener('click', () => {
+//         navMenu.classList.add('showMenu')
+//     })
+// }
+
+
